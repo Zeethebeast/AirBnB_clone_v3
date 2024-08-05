8d787322a20924627bc361c880ab7c3d2e43eee8
zeethebeast <chimasamuel6@gmail.com>
2024-08-05 02:31:32 +0100

    new changes

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,10 +1,24 @@
 #!/usr/bin/python3
+# api/v1/views/index.py
+
+"""
+This module defines the view for the API status endpoint.
+"""
 
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
-@app_views.route('/status', methods=['GET'])
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
-    """Returns the status of the API"""
-    return jsonify({"status": "OK"})
-# api/v1/views/index.py
+    """
+    Returns the status of the API.
+
+    This route returns a JSON object indicating the status of the API.
+    """
+    return jsonify({"status": "OK"}), 200
+
+@app_views.route('/api/v1/stats', methods=['GET'], strict_slashes=False)
+def count():
+    counts = storage.count()
+    return jsonify(counts)

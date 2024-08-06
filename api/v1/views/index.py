a81543c25ba2cfb548b1c67874bae9c58ce268fb
akanSWE <a.akanSWE@gamil.com>
2024-08-06 14:16:17 +0100

    Add `/stats` endpoints

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -15,6 +15,21 @@ from flask import jsonify
 
 
 @app_views.route('/status', methods=['GET'])
-def status():
+def get_status():
     """Returns the status of the API"""
     return jsonify(status="OK")
+
+
+@app_views.route('/stats', methods=['GET'])
+def get_stats():
+    """Counts and returns the number of objects of the mapped classs"""
+    from api import classes
+    from models import storage
+
+    def cls_name_plural(name):
+        """convert mapped class name to it plural form"""
+        name = name.lower()
+        return name.replace('y', 'ies') if name.endswith('y') else name + 's'
+
+    stats_dict = {cls_name_plural(cls): storage.count(cls) for cls in classes}
+    return jsonify(stats_dict)
